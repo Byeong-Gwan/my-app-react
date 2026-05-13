8f6e1dbc05f413d53a94cf9924a459d3aebbfa31
김병관 <ansd_my3073@naver.com>
2026-05-13 10:35:13 +0900

    feat: chapter_04 시계 만들기

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+
+const root = createRoot(document.getElementById('root'))
+setInterval(() =>{
+  root.render(
+    <StrictMode>
+      <Clock />
+    </StrictMode>
+  );
+}, 1000);
+
